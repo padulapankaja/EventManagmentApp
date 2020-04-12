7fb22da307d33e3c0ab8933334f35e3a7426ed98
lucidex <padulaguruge@gmail.com>
2020-04-12 23:11:22 +0530

    add todo calnder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+
+import { FormsModule } from '@angular/forms';
+
+// import time picker
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
+import {NgbDatepickerModule, NgbTimepickerModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxMaterialTimepickerModule.setLocale('ar-AE'),
+    FormsModule,
+    MatSliderModule,
+    NgbTimepickerModule,
+    NgbDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
