9f2a0abba273c903ebee6eba25bde0ac55b9ee87
lucidex <padulaguruge@gmail.com>
2020-04-16 12:24:56 +0530

    not done getnext evnt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Date} from './interfaces/date';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'Online Planner';
+  //greeting title
+  greetingUser : string;
+  ngOnInit(){
+    this.greetingUser  = 'Hello, Welcome ! ';
+    this.greeting();
+  }
+
+  greeting():String{
+    var d = new Date(); // for now
+    d.getHours(); // => 9
+    d.getMinutes(); // =>  30
+    d.getSeconds(); // => 51
+    if(d.getHours() >= 13 && d.getHours() <= 18){
+      this.greetingUser = "Hey, Good Evening";
+    }else if(d.getHours() >= 19){
+      this.greetingUser = "Hey, Good Night"
+    }else if(d.getHours() >= 0 && d.getHours() <= 12){
+      this.greetingUser = "Hey, Good Morning"
+    }
+
+    return this.greetingUser;
+  }
+
+
+  getToday():String{
+    var utc = new Date().toJSON().slice(0,10).replace(/-/g,' - ');
+
+    return utc;
+
+
+  }
 }
--- src/app/components/online-planner/online-planner.component.ts
+++ src/app/components/online-planner/online-planner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgbCalendar, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 // import {stringify} from 'querystring';
 
@@ -19,30 +19,33 @@ export class TodoListComponent implements OnInit {
 
 
   calendar: NgbCalendar;
-  date: {year: number, month: number};
+  date: { year: number, month: number };
 
 
-  currentRate = 5;
+  currentRate = 3;
 
-  events: Event[] ;
+  events: Event[];
   eventId: number;
   eventName: string;
   eventDate: Date;
   eventTime: Time;
   eventExpire: boolean;
   eventDone: boolean;
-  eventEdit:boolean;
-  eventRate:number;
+  eventEdit: boolean;
+  eventRate: number;
   //   before edit
-  beforEditEventName : string;
+  beforEditEventName: string;
 
   //filters
-  filterDay:string;
-  getCalanderDate:Date;
+  filterDay: string;
+  getCalanderDate: Date;
+
+  //addstates
+  addStates: boolean;
 
 
   ngOnInit() {
-    this.filterDay ='all';
+    this.filterDay = 'all';
     this.beforEditEventName = '';
     this.eventId = 4;
     this.eventName = '';
@@ -50,136 +53,183 @@ export class TodoListComponent implements OnInit {
     this.eventTime = null;
     this.eventExpire = false;
     this.eventDone = false;
-    this.eventEdit= false;
-     this.getCalanderDate =null;
-    this.eventRate = 2;
+    this.eventEdit = false;
+    this.getCalanderDate = null;
+    this.eventRate = 3;
+    this.addStates = false;
+
     this.events = [
       {
-        'eventId' : 1,
-        'eventName' : 'This is my first event',
-        'eventDate' : {
-          'year' : '2020',
-          'month' : '04',
-          'day' : '15'
+        'eventId': 1,
+        'eventName': 'This is my first event',
+        'eventDate': {
+          'year': '2020',
+          'month': '04',
+          'day': '15',
+
         },
-        'eventTime' : {
-          'hour' : '22',
-          'minute' : '58',
-          'second' : '0'
+        'eventTime': {
+          'hour': '22',
+          'minute': '58',
+          'second': '0'
         },
-        'eventExpire' : false,
-        'eventDone' : false,
-        'eventEdit' : false
+        'eventExpire': false,
+        'eventDone': false,
+        'eventEdit': false,
+        'eventRate': 3,
       },
       {
-        'eventId' : 2,
-        'eventName' : 'This is my secod event',
-        'eventDate' : {
-          'year' : '2020',
-          'month' : '07',
-          'day' : '05'
+        'eventId': 2,
+        'eventName': 'This is my secod event',
+        'eventDate': {
+          'year': '2020',
+          'month': '07',
+          'day': '05'
         },
-        'eventTime' : {
-          'hour' : '05',
-          'minute' : '58',
-          'second' : '0'
+        'eventTime': {
+          'hour': '05',
+          'minute': '58',
+          'second': '0'
         },
-        'eventExpire' : false,
-        'eventDone' : false,
-        'eventEdit' : false
-
+        'eventExpire': false,
+        'eventDone': false,
+        'eventEdit': false,
+        'eventRate': 3,
       },
       {
-        'eventId' : 3,
-        'eventName' : 'This is my third event',
-        'eventDate' : {
-          'year' : '2020',
-          'month' : '05',
-          'day' : '15'
+        'eventId': 3,
+        'eventName': 'This is my third event',
+        'eventDate': {
+          'year': '2020',
+          'month': '05',
+          'day': '15'
         },
-        'eventTime' : {
-          'hour' : '02',
-          'minute' : '58',
-          'second' : '0'
+        'eventTime': {
+          'hour': '02',
+          'minute': '58',
+          'second': '0'
         },
-        'eventExpire' : false,
-        'eventDone' : false,
-        'eventEdit' : false
-
+        'eventExpire': false,
+        'eventDone': false,
+        'eventEdit': false,
+        'eventRate': 5,
       }
     ];
   }
 
 
-  addEvent(){
-    if (this.eventName.trim().length === 0 || this.eventDate == null || this.eventTime == null){
-      return alert("Please Fill Relevent  Details");
+  addEvent() {
+    if (this.eventName.trim().length === 0 || this.eventDate == null || this.eventTime == null) {
+      return alert('Please Fill Relevent  Details');
     }
 
 
-
     this.events.push({
-      eventId :  this.eventId,
+      eventId: this.eventId,
       eventName: this.eventName,
       eventDate: this.eventDate,
-      eventTime : this.eventTime,
-      eventExpire : false,
-      eventDone : false,
-      eventEdit : false
+      eventTime: this.eventTime,
+      eventExpire: false,
+      eventDone: false,
+      eventEdit: false,
+      eventRate: this.eventRate
 
 
     });
     this.eventName = '';
     this.eventId++;
     console.log(this.events);
+    this.addStates = false;
   }
-  addEventDate(){
-    console.log( this.eventDate );
+
+  addEventDate() {
+    console.log(this.eventDate);
 
   }
-  addEventTime(){
+
+  addEventTime() {
     console.log(this.eventTime);
   }
 
 //  ------------------------- delete event ----------------------
-  deleteEvent(id:number) :void{
-    this.events = this.events.filter(event => event.eventId  !== id);
+  deleteEvent(id: number): void {
+    this.events = this.events.filter(event => event.eventId !== id);
   }
 
 //edit event
-  editEvent(event : Event):void{
+  editEvent(event: Event): void {
     this.beforEditEventName = event.eventName;
     event.eventEdit = true;
   }
-
-  doneEdit(event :Event){
-    if(event.eventName.trim().length === 0){
+  doneEdit(event: Event) {
+    if (event.eventName.trim().length === 0) {
       event.eventName = this.beforEditEventName;
     }
     event.eventEdit = false;
   }
-
-  remaingEvent(): number{
+  remaingEvent(): number {
     return this.events.filter(event => !event.eventDone).length;
   }
-
-  getEventDatebyCalander(){
+  getEventDatebyCalander() {
     console.log(this.getCalanderDate.year);
-    // console.log(this.getCalanderDate.month);
-    // console.log(this.getCalanderDate.day);
-    // this.eventFilterByDay();
   }
 
   eventFilterByDay(): Event[] {
     console.log(this.events);
 
-    if(this.getCalanderDate === null){
-     return this.events
-    }else {
+    if (this.getCalanderDate === null) {
+      return this.events;
+    } else {
       return this.events.filter(event => event.eventDate.year == this.getCalanderDate.year && event.eventDate.month == this.getCalanderDate.month && event.eventDate.day == this.getCalanderDate.day);
     }
+
   }
 
+//--------------get current date
+  getcurrentDate() {
+    var currentDate = new Date();
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1;
+    var year = currentDate.getFullYear();
+
+    var tempCurrentDate = {
+      'year': year,
+      'month': month,
+      'day': day
+    };
+
+    console.log('Cuurent Date');
+    console.log(tempCurrentDate);
+    return tempCurrentDate;
+
+  }
+
+  checkeventExpireornot(eventDate, currentDate): boolean {
+    if (eventDate.year <= currentDate.year && eventDate.month <= currentDate.month && eventDate.day <= currentDate.day) {
+      return true;
+    }
+    return false;
+  }
+
+  getAddStatus(): boolean {
+    return this.addStates;
+  }
+  changeAddStateFalse(): boolean {
+
+    return this.addStates = false;
+  }
+
+  changeStat(): boolean {
+    if (this.addStates === true) {
+      return this.addStates = false;
+    } else {
+      return this.addStates = true;
+    }
+  }
+  getnextEvent(){
+
+
+}
 
 
 }
